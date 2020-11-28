782a79c268c9f6764fc091e38a90be834bdfbaa4
Lars Rødal <lrodal@maritech.no>
2020-11-29 00:43:15 +0100

    initial touch support, added padding around btns, added dbl click on canvas for drop

--- src/app/board.component.ts
+++ src/app/board.component.ts
@@ -147,7 +147,7 @@ export class BoardComponent implements AfterViewInit {
   initBoard() {
     this.ctx = this.canvas.nativeElement.getContext('2d');
 
-    const height = (this.wrapper.nativeElement as HTMLDivElement).getBoundingClientRect().height - 130;
+    const height = (this.wrapper.nativeElement as HTMLDivElement).getBoundingClientRect().height - 150;
     const width = (this.wrapper.nativeElement as HTMLDivElement).getBoundingClientRect().width;
 
     this.blockSize = Math.min(width / COLS, height / ROWS);
@@ -196,6 +196,58 @@ export class BoardComponent implements AfterViewInit {
       this.audio[this.config.selectedMusicTheme].play();
       this.audio[this.config.selectedMusicTheme].volume = 1;
     }
+  }
+
+  swipedir;
+  dist;
+  startX;
+  startY;
+  startTime;
+  distX;
+  distY;
+  elapsedTime;
+  threshold = 150; //required min distance traveled to be considered swipe
+  restraint = 100; // maximum distance allowed at the same time in perpendicular direction
+  allowedTime = 300; // maximum time allowed to travel that distance
+  canvasTouchStarted(e) {
+    const touchobj = e.changedTouches[0];
+    this.swipedir = 'none';
+    this.dist = 0;
+    this.startX = touchobj.pageX;
+    this.startY = touchobj.pageY;
+    this.startTime = new Date().getTime(); // record time when finger first makes contact with surface
+    e.preventDefault();
+  }
+
+  canvasTouchEnded(e) {
+    const touchobj = e.changedTouches[0];
+    this.distX = touchobj.pageX - this.startX; // get horizontal dist traveled by finger while in contact with surface
+    this.distY = touchobj.pageY - this.startY; // get vertical dist traveled by finger while in contact with surface
+    this.elapsedTime = new Date().getTime() - this.startTime; // get time elapsed
+    if (this.elapsedTime <= this.allowedTime) {
+      // first condition for awipe met
+      if (Math.abs(this.distX) >= this.threshold && Math.abs(this.distY) <= this.restraint) {
+        // 2nd condition for horizontal swipe met
+        this.swipedir = this.distX < 0 ? 'left' : 'right'; // if dist traveled is negative, it indicates left swipe
+      } else if (Math.abs(this.distY) >= this.threshold && Math.abs(this.distX) <= this.restraint) {
+        // 2nd condition for vertical swipe met
+        this.swipedir = this.distY < 0 ? 'up' : 'down'; // if dist traveled is negative, it indicates up swipe
+      }
+    }
+    this.handleSwipe(this.swipedir);
+    e.preventDefault();
+  }
+
+  handleSwipe(swipe: 'up' | 'down' | 'left' | 'right') {
+    swipe === 'up'
+      ? this.move(38)
+      : swipe === 'down'
+      ? this.move(40)
+      : swipe === 'left'
+      ? this.move(37)
+      : swipe === 'right'
+      ? this.move(39)
+      : false;
   }
 
   resetGame() {
