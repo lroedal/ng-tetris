86a4c1a57b63a971387f03a1eb3bb12508a7b1c6
Lars Rødal <lrodal@maritech.no>
2020-11-30 01:34:40 +0100

    removed unused enum

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -71,15 +71,6 @@ export const SHAPES = [
   ],
 ];
 
-export class KEY {
-  static readonly ESC = 27;
-  static readonly SPACE = 32;
-  static readonly LEFT = 37;
-  static readonly UP = 38;
-  static readonly RIGHT = 39;
-  static readonly DOWN = 40;
-}
-
 export class POINTS {
   static readonly SINGLE = 100;
   static readonly DOUBLE = 300;
